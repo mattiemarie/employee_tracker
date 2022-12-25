79b1846e9fea8c1ba1616848b394f50b817108e5
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-24 20:59:45 -0500

    Adding Main Choice

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001
@@ -17,4 +18,41 @@ const db = mysql.createConnection (
         database: 'employee_db'
     },
     console.log(`Connected to the employee_db Database.`)
+
 );
+
+mainChoice();
+
+// Main Choice for Employee Database
+function mainChoice() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'mainChoice',
+                message: 'What would you like to do?',
+                choices:['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+             }
+        ]).then (function(data) {
+            switch(data.mainChoice) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+            };
+        });
+};
